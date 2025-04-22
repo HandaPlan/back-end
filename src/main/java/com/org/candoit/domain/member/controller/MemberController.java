7805eb7fcde120952228bc5e9e4713544901ec3c
zzuharchive <welcome-ing@naver.com>
2025-04-22 21:36:26 +0900

    feature: 닉네임, 이메일 중복 체크 기능 구현

--- src/main/java/com/org/candoit/domain/member/controller/MemberController.java
+++ src/main/java/com/org/candoit/domain/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.org.candoit.domain.member.controller;
 
+import com.org.candoit.domain.member.dto.MemberCheckRequest;
 import com.org.candoit.domain.member.dto.MemberJoinRequest;
 import com.org.candoit.domain.member.service.MemberService;
 import com.org.candoit.global.response.ApiResponse;
@@ -21,5 +22,11 @@ public class MemberController {
     public ResponseEntity<ApiResponse<Object>> join(@RequestBody MemberJoinRequest memberJoinRequest){
         memberService.join(memberJoinRequest);
         return ResponseEntity.ok(ApiResponse.successWithoutData());
+    }
+
+    @PostMapping("/check")
+    public ResponseEntity<ApiResponse<Boolean>> check(@RequestBody MemberCheckRequest memberCheckRequest){
+        Boolean result = memberService.check(memberCheckRequest);
+        return ResponseEntity.ok(ApiResponse.success(result));
     }
 }
--- src/main/java/com/org/candoit/domain/member/service/MemberService.java
+++ src/main/java/com/org/candoit/domain/member/service/MemberService.java
@@ -1,8 +1,11 @@
 package com.org.candoit.domain.member.service;
 
+import com.org.candoit.domain.member.dto.MemberCheckRequest;
 import com.org.candoit.domain.member.dto.MemberJoinRequest;
 import com.org.candoit.domain.member.entity.Member;
 import com.org.candoit.domain.member.repository.MemberRepository;
+import com.org.candoit.global.response.CustomException;
+import com.org.candoit.global.response.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,5 +26,19 @@ public class MemberService {
             .build();
 
         memberRepository.save(saveRequestMember);
+    }
+
+    public Boolean check(MemberCheckRequest memberCheckRequest){
+
+        String type = memberCheckRequest.getType();
+        String content = memberCheckRequest.getContent();
+
+        if(type.equals("nickname")){
+            return memberRepository.findByNickname(content).isPresent();
+        }else if(type.equals("email")){
+            return memberRepository.findByEmail(content).isPresent();
+        }else {
+            throw new CustomException(ErrorCode.BAD_REQUEST);
+        }
     }
 }
