398ceaac040d891810a99c4caaefad26b327bd0e
zzuharchive <welcome-ing@naver.com>
2025-05-10 14:29:10 +0900

    fix: 탈퇴한 사용자가 로그인 요청 시, 예외 발생

--- src/main/java/com/org/candoit/global/security/basic/CustomUserDetailsService.java
+++ src/main/java/com/org/candoit/global/security/basic/CustomUserDetailsService.java
@@ -1,10 +1,12 @@
 package com.org.candoit.global.security.basic;
 
 import com.org.candoit.domain.member.entity.Member;
+import com.org.candoit.domain.member.entity.MemberStatus;
 import com.org.candoit.domain.member.exception.MemberErrorCode;
 import com.org.candoit.domain.member.repository.MemberRepository;
 import com.org.candoit.global.response.CustomException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,10 @@ public class CustomUserDetailsService implements UserDetailsService {
 
         Member findMember = memberRepository.findByEmail(username).orElseThrow(()->new CustomException(
             MemberErrorCode.NOT_FOUND_MEMBER));
+
+        if(findMember.getMemberStatus() == MemberStatus.WITHDRAW){
+            throw new DisabledException(MemberErrorCode.WITHDRAWN_MEMBER.getMessage());
+        }
 
         return new CustomUserDetails(findMember);
     }
