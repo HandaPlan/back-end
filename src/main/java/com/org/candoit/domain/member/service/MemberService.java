20018b1a89295907a2c0e3647ed0e5c1e3472414
zzuharchive <welcome-ing@naver.com>
2025-07-21 19:23:56 +0900

    fix: 서비스 레이어에서 사용자 탈퇴 부분 수정

--- src/main/java/com/org/candoit/domain/member/service/MemberService.java
+++ src/main/java/com/org/candoit/domain/member/service/MemberService.java
@@ -14,10 +14,7 @@ import com.org.candoit.domain.member.repository.MemberRepository;
 import com.org.candoit.global.response.CustomException;
 import com.org.candoit.global.response.GlobalErrorCode;
 import java.time.Duration;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -145,7 +142,7 @@ public class MemberService {
 
         Member memberToWithdraw = memberRepository.findById(memberId).orElseThrow(()->new CustomException(MemberErrorCode.NOT_FOUND_MEMBER));
 
-        checkVerify(memberId, "withdraw");
+        checkVerify(memberId, checkPasswordRequest.getType());
 
         memberToWithdraw.withdraw();
     }
