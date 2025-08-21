f82ffa07205a36c9d79bf6c987704f89c719e081
zzuharchive <welcome-ing@naver.com>
2025-08-21 14:14:26 +0900

    feature: 서브골 생성 api 구현 및 서비스 테스트 코드 작성

---
+++ src/main/java/com/org/candoit/domain/subgoal/controller/SubGoalController.java
@@ -0,0 +1,35 @@
+package com.org.candoit.domain.subgoal.controller;
+
+import com.org.candoit.domain.member.entity.Member;
+import com.org.candoit.domain.subgoal.dto.CreateSubGoalRequest;
+import com.org.candoit.domain.subgoal.dto.SimpleSubGoalInfoResponse;
+//import com.org.candoit.domain.subgoal.service.SubGoalService;
+import com.org.candoit.domain.subgoal.service.SubGoalService;
+import com.org.candoit.global.annotation.LoginMember;
+import com.org.candoit.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class SubGoalController {
+
+    private final SubGoalService subGoalService;
+
+    @PostMapping("/main-goals/{mainGoalId}/sub-goals")
+    public ResponseEntity<ApiResponse<SimpleSubGoalInfoResponse>> createSubGoal(@Parameter(hidden = true) @LoginMember Member loginMember,
+        @PathVariable Long mainGoalId,  @Valid @RequestBody CreateSubGoalRequest createSubGoalRequest) {
+
+        SimpleSubGoalInfoResponse result = subGoalService.createSubGoal(loginMember, mainGoalId, createSubGoalRequest);
+        return ResponseEntity.ok(ApiResponse.success(result));
+    }
+}
