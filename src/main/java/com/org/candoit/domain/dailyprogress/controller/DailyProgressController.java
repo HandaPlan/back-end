46dd60e543cf99535de803bb2dc733e92e5a0fd0
zzuharchive <welcome-ing@naver.com>
2025-09-17 18:29:07 +0900

    fix: dailyAction check api의 http 메소드를 post에서 put으로 변경

--- src/main/java/com/org/candoit/domain/dailyprogress/controller/DailyProgressController.java
+++ src/main/java/com/org/candoit/domain/dailyprogress/controller/DailyProgressController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,7 @@ public class DailyProgressController {
 
     private final DailyProgressService dailyProgressService;
 
-    @PostMapping("/sub-goals/{subGoalId}/daily-progress")
+    @PutMapping ("/sub-goals/{subGoalId}/daily-progress")
     public ResponseEntity<ApiResponse<List<DetailProgressResponse>>> checkedDate(
         @Parameter(hidden = true) @LoginMember Member loginMember,
         @PathVariable Long subGoalId,
