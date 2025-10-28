f5644dc9edc5320557b54bdc202429659f811f7f
zzuharchive <welcome-ing@naver.com>
2025-10-28 16:15:43 +0900

    fix: 회원 정보 수정 시, 프로필 이미지 업데이트 로직 추가

--- src/main/java/com/org/candoit/domain/member/dto/MemberUpdateRequest.java
+++ src/main/java/com/org/candoit/domain/member/dto/MemberUpdateRequest.java
@@ -1,12 +1,11 @@
 package com.org.candoit.domain.member.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class MemberUpdateRequest {
 
-    private String profile_image;
+    private String profileImage;
     private String comment;
     private String nickname;
 }
